8ac0fcf2dd25c8f7ddc253fad7f65923a81cb57d
Eduardo Alvarado <33001422+edualvarado@users.noreply.github.com>
2020-04-17 22:00:11 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,7 +36,7 @@ var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
-    document.getElementById("header_nav").style.top = "-50px";
+    document.getElementById("header_nav").style.top = "0";
   } else {
     document.getElementById("header_nav").style.top = "-50px";
   }
