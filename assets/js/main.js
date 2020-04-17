74c120efdd2844e7ffbd7851f72a72293f949d10
Eduardo Alvarado <33001422+edualvarado@users.noreply.github.com>
2020-04-17 18:32:19 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -40,15 +40,3 @@ function myFunction() {
     x.className = "topnav";
   }
 }
-
-/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("nav").style.top = "0";
-  } else {
-    document.getElementById("nav").style.top = "-50px";
-  }
-  prevScrollpos = currentScrollPos;
-}
