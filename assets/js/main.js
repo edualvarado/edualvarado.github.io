517c94f7d0c83229fa1bc6bf52b05c9561ab66ef
Eduardo Alvarado <33001422+edualvarado@users.noreply.github.com>
2020-04-18 12:53:21 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -44,3 +44,13 @@ window.onscroll = function() {
   prevScrollpos = currentScrollPos;
 }
 */
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("navbar");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
