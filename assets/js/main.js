2969869028e4753bfd07fffb46eb3187d400c037
Eduardo Alvarado <33001422+edualvarado@users.noreply.github.com>
2020-04-19 20:00:17 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,3 +30,13 @@
 		$('.scrolly').scrolly();
 
 })(jQuery);
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("nav");
+  if (x.className === "header_nav") {
+    x.className += " responsive";
+  } else {
+    x.className = "header_nav";
+  }
+}
