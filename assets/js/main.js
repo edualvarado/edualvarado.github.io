b7771c893dc33944db07ee9a5f3e884d711037be
Eduardo Alvarado <33001422+edualvarado@users.noreply.github.com>
2020-04-20 17:33:53 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -64,8 +64,8 @@ function filterSelection(c) {
   if (c == "all") c = "";
   // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
   for (i = 0; i < x.length; i++) {
-    w3RemoveClass(x[i], "show");
-    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+    w3RemoveClass(x[i], "show_portfolio");
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show_portfolio");
   }
 }
 
