9a658157d60f44a8edee2890bdcb1fe5435d2cc1
Eduardo Alvarado <33001422+edualvarado@users.noreply.github.com>
2020-04-28 15:33:34 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -128,3 +128,38 @@ span.onclick = function() {
   modal.style.display = "none";
 }
 
+/* Scroll for menu ----- DRAFT */
+
+$(document).ready(function () {
+    // Read the cookie and if it's defined scroll to id
+    var scroll = $.cookie('scroll');
+    if(scroll){
+        scrollToID(scroll, 1000);
+        $.removeCookie('scroll');
+    }
+
+    // Handle event onclick, setting the cookie when the href != #
+    $('.nav a').click(function (e) {
+        e.preventDefault();
+        var id = $(this).data('id');
+        var href = $(this).attr('href');
+        if(href === '#'){
+            scrollToID(id, 1000);
+        }else{
+            $.cookie('scroll', id);
+            window.location.href = href;
+        }
+    });
+
+    // scrollToID function
+    function scrollToID(id, speed) {
+        var offSet = 70;
+        var obj = $('#' + id);
+        if(obj.length){
+          var offs = obj.offset();
+          var targetOffset = offs.top - offSet;
+          $('html,body').animate({ scrollTop: targetOffset }, speed);
+        }
+    }
+});
+
