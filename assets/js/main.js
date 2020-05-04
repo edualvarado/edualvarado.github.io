04acf493f564915c5ac0ee092949a9cf35e4deac
Eduardo Alvarado <33001422+edualvarado@users.noreply.github.com>
2020-05-04 13:38:17 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -127,3 +127,21 @@ var span = document.getElementsByClassName("close")[0];
 span.onclick = function() {
   modal.style.display = "none";
 }
+
+/* Button UP ----- DRAFT */
+$(document).ready(function(){
+	$(window).scroll(function () {
+			if ($(this).scrollTop() > 50) {
+				$('#back-to-top').fadeIn();
+			} else {
+				$('#back-to-top').fadeOut();
+			}
+		});
+		// scroll body to 0px on click
+		$('#back-to-top').click(function () {
+			$('body,html').animate({
+				scrollTop: 0
+			}, 400);
+			return false;
+		});
+});
