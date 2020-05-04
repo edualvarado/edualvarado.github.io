fb87d2f3d9c86e446eaae7b70a0e3627c16daf46
Eduardo Alvarado <33001422+edualvarado@users.noreply.github.com>
2020-05-04 13:53:02 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -128,20 +128,19 @@ span.onclick = function() {
   modal.style.display = "none";
 }
 
-/* Button UP ----- DRAFT */
-$(document).ready(function(){
-	$(window).scroll(function () {
-			if ($(this).scrollTop() > 50) {
-				$('#back-to-top').fadeIn();
-			} else {
-				$('#back-to-top').fadeOut();
-			}
-		});
-		// scroll body to 0px on click
-		$('#back-to-top').click(function () {
-			$('body,html').animate({
-				scrollTop: 0
-			}, 400);
-			return false;
-		});
+/* top_button ----- DRAFT */
+
+var btn = $('#top_button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
 });
