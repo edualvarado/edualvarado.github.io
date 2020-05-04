4f214cc254ff01cf28973b68faaa956950f1b56d
Eduardo Alvarado <33001422+edualvarado@users.noreply.github.com>
2020-05-04 13:58:17 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -128,19 +128,4 @@ span.onclick = function() {
   modal.style.display = "none";
 }
 
-/* top_button ----- DRAFT */
-
-var btn = $('#top_button');
-
-$(window).scroll(function() {
-  if ($(window).scrollTop() > 300) {
-    btn.addClass('show');
-  } else {
-    btn.removeClass('show');
-  }
-});
-
-btn.on('click', function(e) {
-  e.preventDefault();
-  $('html, body').animate({scrollTop:0}, '300');
-});
+/* top_button ----- TODO */
