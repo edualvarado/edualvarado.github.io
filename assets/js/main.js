610877f8e340f65b4fddf79ea53a801981267094
Eduardo Alvarado <33001422+edualvarado@users.noreply.github.com>
2020-12-29 19:08:30 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -45,6 +45,22 @@ window.addEventListener('resize', () => {
   }
 })
 
+// NEW!!!
+
+// When the user scrolls down 20px from the top of the document, slide down the navbar
+// When the user scrolls to the top of the page, slide up the navbar (50px out of the top view)
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("nav").style.top = "0";
+  } else {
+    document.getElementById("nav").style.top = "-50px";
+  }
+}
+
+//
+
 /* Portfolio ----- Not used */
 
 filterSelection("all") // Execute the function and show all columns
