25fa4edf7460ba65c34ba75bfb4133cfdea9d903
Eduardo Alvarado <33001422+edualvarado@users.noreply.github.com>
2020-12-30 11:45:49 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -53,9 +53,9 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("nav").style.top = "0";
+    document.getElementById("navbar").style.top = "0";
   } else {
-    document.getElementById("nav").style.top = "-50px";
+    document.getElementById("navbar").style.top = "-50px";
   }
 }
 
